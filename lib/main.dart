a8b2537d5becc11d29a997295e59df15d5954abb
simran patwal <simranp@athmin.com>
2020-03-29 11:11:38 +0530

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,2 @@
-import 'package:flutter/material.dart';
-//import 'package:provider/provider.dart';
-import 'package:vinnoba/screens/login_page.dart';
-//import './data/post_api_service.dart';
-//import 'homepage.dart';
-
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget{
-
-  @override
-  Widget build(BuildContext context) {
-
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Material App",
-      home: Login(),
-    );
-  }
-
-}
-
-
-    //Provider(
-      //builder: (_) => PostApiService.create(),
-      //dispose: (_, PostApiService service) => service.client.dispose(),
-      //child:
 
 
